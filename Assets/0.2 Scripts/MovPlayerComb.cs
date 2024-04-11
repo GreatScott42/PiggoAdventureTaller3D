6479f8fe7a8290d44866c3fe8c77f4a407018c8c
GreatScott42 <58761187+GreatScott42@users.noreply.github.com>
2024-04-11 17:02:07 -0400

    salto, dash y vida
    -el salto funciona correctamente con espacio
    -el dash funciona correctamente con shift, agregar cooldown y quiza mas fuerza?
    -para el sistema de vida agregue una imagen en un canvas que va disminuyendo al presionar h (cambiar para que baje al ser atacado), quiza modificar?

--- Assets/0.2 Scripts/MovPlayerComb.cs
+++ Assets/0.2 Scripts/MovPlayerComb.cs
@@ -10,9 +10,13 @@ public class MovPlayerComb : MonoBehaviour
     private Vector3 moveDirection;
     private float fixedY;
     private Rigidbody rb;
+    private Vector3 dashCoords;
+    public GameObject lifeBar;
+    private int lifeBarInt;
 
     void Start()
     {
+        lifeBarInt = 200;
         fixedY = 45f;
         rb = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
@@ -22,7 +26,7 @@ public class MovPlayerComb : MonoBehaviour
         //transform.eulerAngles = new Vector3(0,Input.GetAxis("Horizontal"),0);
         //if()
         //direccion a la cual se movera
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"),0f, Input.GetAxis("Vertical")).normalized;
         //moveDirection = transform.TransformDirection(moveDirection);
         
 
@@ -32,12 +36,13 @@ public class MovPlayerComb : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(moveDirection);
         //hacerlo mas lento, porque va muy rapido
         rotation = Quaternion.RotateTowards(transform.rotation, rotation, 360 * Time.deltaTime);
+        rb.MoveRotation(rotation);
         //agregar la gravedad
         moveDirection *= speed;
-        moveDirection.y = rb.velocity.y;
+        //moveDirection.y = rb.velocity.y;
         //aplicar rotacion y movimiento
-        rb.MoveRotation(rotation);
-        rb.MovePosition(rb.position + (Quaternion.AngleAxis(fixedY, Vector3.up) * moveDirection) * 2 * Time.deltaTime);
+        
+        rb.MovePosition(rb.position + (Quaternion.AngleAxis(fixedY, Vector3.up) * moveDirection)*2 * Time.deltaTime);
         //controller.Move(moveDirection * Time.deltaTime);
 
         
@@ -47,6 +52,30 @@ public class MovPlayerComb : MonoBehaviour
 
     void Update()
     {
+        //salto
+        if (Input.GetKeyDown(KeyCode.Space)&&rb.velocity.y==0)
+        {
+            rb.AddForce(new Vector3(0, 400f, 0),ForceMode.Force);
+        }
+        if (rb.velocity.y > 0)
+        {
+            //Debug.Log(rb.velocity.y);
+        }
         
+        
+        //dash
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dashCoords = new Vector3(Mathf.Sin(transform.localRotation.eulerAngles.y * Mathf.Deg2Rad), 0, Mathf.Cos(transform.localRotation.eulerAngles.y * Mathf.Deg2Rad));
+            dashCoords = Quaternion.AngleAxis(fixedY, Vector3.up)*dashCoords;
+            rb.AddForce(dashCoords*10f,ForceMode.VelocityChange);
+            //Debug.Log("RotationY: "+transform.localRotation.eulerAngles.y+" / VectorX: "+dashCoords.x+" / VectorZ: "+ dashCoords.z);
+        }
+        //quitar vida
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            lifeBarInt -= 40;
+            lifeBar.GetComponent<RectTransform>().sizeDelta = new Vector2(lifeBarInt,25);
+        }
     }
 }
